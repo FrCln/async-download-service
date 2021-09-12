3891d90f043910efa68b948a19fa8162d2d91388
AnatoliyChistov <atchistov@gmail.com>
2021-09-12 20:53:37 +0300

    task complete

---
+++ run_server.sh
@@ -0,0 +1,4 @@
+export DOWNLOAD_SERVICE_LOGGING_LEVEL=10
+export DOWNLOAD_SERVICE_PAUSE=0.1
+export DOWNLOAD_SERVICE_PATH=test_photos
+python server.py
--- server.py
+++ server.py
@@ -1,9 +1,70 @@
+import asyncio
+import logging
+import os
+
 from aiohttp import web
 import aiofiles
 
 
+try:
+    LOGGING_LEVEL = logging._nameToLevel[os.getenv('DOWNLOAD_SERVICE_LOGGING_LEVEL')]
+except KeyError:
+    LOGGING_LEVEL = logging.INFO
+
+try:
+    PAUSE = float(os.getenv('DOWNLOAD_SERVICE_PAUSE'))
+except ValueError:
+    PAUSE = None
+
+PHOTO_PATH = os.getenv('DOWNLOAD_SERVICE_PATH', 'test_photos2')
+
+
+logging.basicConfig(level=LOGGING_LEVEL)
+
+
 async def archivate(request):
-    raise NotImplementedError
+    response = web.StreamResponse()
+
+    archive_hash = request.match_info.get('archive_hash')
+    cwd = os.getcwd()
+    dirname = os.path.join(PHOTO_PATH, archive_hash)
+
+    if not os.path.exists(dirname):
+        raise web.HTTPNotFound(body="Архив не существует или был удален".encode('utf-8'))
+
+    process = await asyncio.create_subprocess_exec(
+        '/usr/bin/zip', '-rj', '-', dirname,
+        stdout=asyncio.subprocess.PIPE
+    )
+
+    response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
+
+    # Отправляет клиенту HTTP заголовки
+    logging.debug("Sending response headers ...")
+    await response.prepare(request)
+
+    try:
+        logging.info("Archive sending started ...")
+        while True:
+            buf = await process.stdout.read(4096)
+            if not buf:
+                break
+
+            logging.debug("Sending archive chunk ...")
+            await response.write(buf)
+            if PAUSE:
+                await asyncio.sleep(PAUSE)
+            
+    except (asyncio.CancelledError, ConnectionResetError):
+        logging.info("Download was interrupted by user")
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+    finally:
+        if process.returncode is None:
+            process.kill()
+            process.communicate()
+
+    return response
 
 
 async def handle_index_page(request):
